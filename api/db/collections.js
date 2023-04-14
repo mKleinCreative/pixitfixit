5a21a74ae08363743d0f46afcc7b4581a984845d
David Ogbonna <davidcogbonna35@gmail.com>
2023-04-13 22:40:40 -0400

    Created Collection and User Schema

---
+++ api/db/collections.js
@@ -0,0 +1,18 @@
+import { dbConnection } from "./connect.js";
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+// NOTE: YOU WILL NEED TO CHANGE THE CODE BELOW TO HAVE THE COLLECTION(S) REQUIRED BY THE ASSIGNMENT
+// export const posts = getCollectionFn('posts');
+export const users = getCollectionFn("users");
--- api/models/users.js
+++ api/models/users.js
@@ -1,14 +1,38 @@
-const createUser = async (
-    firstName,
-    LastName,
-    Email,
-    password,
-    Role,
-    comment,
-    potholes,
-    username,
-    Age
-  ) => {
+import { user } from "../_db/collection.js";
 
-    
+const createUser = async (
+  firstName,
+  LastName,
+  Email,
+  address,
+  password,
+  Role,
+  comment,
+  potholes,
+  username,
+  Age
+) => {
+  let newUser = {
+    firstName: firstName,
+    LastName: LastName,
+    Email: Email,
+    address: address,
+    password: password,
+    Role: Role,
+    comment: comment,
+    potholes: potholes,
+    username: username,
+    Age: Age,
+  };
+
+  const usercollection = await user();
+  const insertedUser = await usercollection.insertOne(newUser);
+
+  if (insertedUser.acknowledged === true) {
+    console.log(newUser);
+    return newUser;
+  } else {
+    throw "this did not work!";
   }
+};
+export default createUser;
