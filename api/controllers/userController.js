daa6d977f01a5b1257aedd413efd419ec401fdee
George Ayob <georgeayob@georges-air.myfiosgateway.com>
2023-04-22 13:52:00 -0400

    includes functions for creating, checking, finding, changing permissions, restricting, checking if a user is an admin, and deleting a user. The user object now has also the new attributes

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -5,134 +5,140 @@ const saltRounds = 10;
 
 const usernameRegex = /^[a-zA-Z0-9]{4,}$/;
 const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{6,}$/;
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 const createUser = async (
+  firstName,
+  lastName,
+  email,
+  password,
+  role,
+  comment,
+  assignedPotholes,
+  potholesCreated,
+  username,
+  birthday
+) => {
+  if (!email || typeof email !== 'string' || !emailRegex.test(email)) {
+    throw new Error('Email must be a valid email address');
+  }
+
+  if (!password || typeof password !== 'string' || !passwordRegex.test(password)) {
+    throw new Error('Password must be a non-empty string with at least 6 characters, including one uppercase character, one number, and one special character');
+  }
+
+  if (!username || typeof username !== 'string' || !usernameRegex.test(username)) {
+    throw new Error('Username must be a non-empty alphanumeric string with at least 4 characters and no spaces');
+  }
+
+  const users = await userCollection();
+  const userExists = await users.findOne({ email: email.toLowerCase() });
+
+  if (userExists) {
+    throw new Error('Email already exists');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  const newUser = {
     firstName,
     lastName,
-    email,
-    password,
+    email: email.toLowerCase(),
+    password: hashedPassword,
     role,
     comment,
-    potholes,
+    assignedPotholes,
+    potholesCreated,
     username,
-    age
-  ) => {
-    if (!email || typeof email !== 'string' || !emailRegex.test(email)) {
-      throw new Error('Email must be a valid email address');
-    }
-  
-    if (!password || typeof password !== 'string' || !passwordRegex.test(password)) {
-      throw new Error('Password must be a non-empty string with at least 6 characters, including one uppercase character, one number, and one special character');
-    }
-  
-    if (!username || typeof username !== 'string' || !usernameRegex.test(username)) {
-      throw new Error('Username must be a non-empty alphanumeric string with at least 4 characters and no spaces');
-    }
-  
-    const users = await userCollection();
-    const userExists = await users.findOne({ email: email.toLowerCase() });
-  
-    if (userExists) {
-      throw new Error('Email already exists');
-    }
-  
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-  
-    const newUser = {
-      firstName,
-      lastName,
-      email: email.toLowerCase(),
-      password: hashedPassword,
-      role,
-      comment,
-      potholes,
-      username,
-      age,
-    };
-  
-    const insertInfo = await users.insertOne(newUser);
-    if (insertInfo.insertedCount === 0) {
-      throw new Error('Failed to create user');
-    }
-  
-    return { insertedUser: true };
+    birthday,
+    restricted: false
   };
 
-  
-  const checkUser = async (email, password) => {
-    if (!email || typeof email !== 'string' || !emailRegex.test(email)) {
-      throw new Error('Email must be a valid email address');
-    }
-  
-    if (!password || typeof password !== 'string' || !passwordRegex.test(password)) {
-      throw new Error('Password must be a non-empty string with at least 6 characters, including one uppercase character, one number, and one special character');
-    }
-  
-    const users = await userCollection();
-    const user = await users.findOne({ email: email.toLowerCase() });
-  
-    if (!user) {
-      throw new Error('Either the email or password is invalid');
-    }
-  
-    const passwordMatches = await bcrypt.compare(password, user.password);
-  
-    if (!passwordMatches) {
-      throw new Error('Either the email or password is invalid');
-    }
-  
-    return { authenticatedUser: true };
-  };
-  
-  const findUserByEmail = async (email) => {
-    if (!email || typeof email !== 'string' || !emailRegex.test(email)) {
-      throw new Error('Email must be a valid email address');
-    }
-  
-    const users = await userCollection();
-    return await users.findOne({ email: email.toLowerCase() });
-  };
-  
-  const findUserByUsername = async (username) => {
-    if (!username || typeof username !== 'string' || !usernameRegex.test(username)) {
-      throw new Error('Username must be a non-empty alphanumeric string with at least 4 characters and no spaces');
-    }
-  
-    const users = await userCollection();
-    return await users.findOne({ username });
-  };
-  
-  const changeRole = async (email, role) => {
-    const user = await findUserByEmail(email);
-  
-    if (!user) {
-      throw new Error('User not found');
-    }
-  
-    const users = await userCollection();
-    return await users.updateOne({ _id: user._id }, { $set: { role } });
-  };
-  
-  const deleteUser = async (email) => {
-    const user = await findUserByEmail(email);
-  
-    if (!user) {
-      throw new Error('User not found');
-    }
-  
-    const users = await userCollection();
-    return await users.deleteOne({ _id: user._id });
-  };
-  
-  module.exports = {
-    createUser,
-    checkUser,
-    findUserByEmail,
-    findUserByUsername,
-    changeRole,
-    deleteUser,
-  };
-  
+  const insertInfo = await users.insertOne(newUser);
+  if (insertInfo.insertedCount === 0) {
+    throw new Error('Failed to create user');
+  }
+
+  return { insertedUser: true };
+};
+
+const checkUser = async (email, password) => {
+  if (!email || typeof email !== 'string' || !emailRegex.test(email)) {
+    throw new Error('Email must be a valid email address');
+  }
+
+  if (!password || typeof password !== 'string' || !passwordRegex.test(password)) {
+    throw new Error('Password must be a non-empty string with at least 6 characters, including one uppercase character, one number, and one special character');
+  }
+
+  const users = await userCollection();
+  const user = await users.findOne({ email: email.toLowerCase() });
+
+  if (!user) {
+    throw new Error('Either the email or password is invalid');
+  }
+
+  const passwordMatches = await bcrypt.compare(password, user.password);
+
+  if (!passwordMatches) {
+    throw new Error('Either the email or password is invalid');
+  }
+
+  return { authenticatedUser: true };
+};
+
+const FindUser = async (email) => {
+  if (!email || typeof email !== 'string' || !emailRegex.test(email)) {
+    throw new Error('Email must be a valid email address');
+  }
+
+  const users = await userCollection();
+  return await users.findOne({ email: email.toLowerCase() });
+};
+
+const ChangePermissions = async (email, role) => {
+  const user = await FindUser(email);
+
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  const users = await userCollection();
+  return await users.updateOne({ _id: user._id },
+    { $set: { role } });
+};
+
+const RestrictUser = async (email) => {
+const user = await FindUser(email);
+
+if (!user) {
+throw new Error('User not found');
+}
+
+const users = await userCollection();
+return await users.updateOne({ _id: user._id }, { $set: { restricted: true } });
+};
+
+const AdminCheck = async (email) => {
+const user = await FindUser(email);
+
+if (!user) {
+throw new Error('User not found');
+}
+
+return user.role === 'admin';
+};
+
+const DeleteUser = async (email) => {
+const user = await FindUser(email);
+
+if (!user) {
+throw new Error('User not found');
+}
+
+const users = await userCollection();
+return await users.deleteOne({ _id: user._id });
+};
 
 module.exports = {
 createUser,
@@ -141,7 +147,5 @@ FindUser,
 ChangePermissions,
 RestrictUser,
 AdminCheck,
-DeleteUser
+DeleteUser,
 };
-   
- 
