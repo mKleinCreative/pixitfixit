a1bc4ad6b573568a6f4579f8254a1ace68837978
David Ogbonna <davidcogbonna35@gmail.com>
2023-04-26 12:36:57 -0400

    These are commented files for test

--- api/models/potholes.js
+++ api/models/potholes.js
@@ -1,32 +1,32 @@
 
-const createPothole = async (
-  users_id,
-  status,
-  photo_url,
-  Lat,
-  Long,
-  comment
-) => {
-    // check to see of if the user_id is present in DB!!! --> low priotiry! 
+// const createPothole = async (
+//   users_id,
+//   status,
+//   photo_url,
+//   Lat,
+//   Long,
+//   comment
+// ) => {
+//     // check to see of if the user_id is present in DB!!! --> low priotiry! 
 
-  let newPothole = {
-    users_id: users_id,
-    status: status,
-    photo_url: photo_url,
-    Lat: Lat,
-    Long: Long,
-    Role: Role,
-    comment: comment,
-  };
+//   let newPothole = {
+//     users_id: users_id,
+//     status: status,
+//     photo_url: photo_url,
+//     Lat: Lat,
+//     Long: Long,
+//     Role: Role,
+//     comment: comment,
+//   };
 
-  const poteHolecollection = await pothole();
-  const insertedPotHole = await poteHolecollection.insertOne(newPothole);
+//   const poteHolecollection = await pothole();
+//   const insertedPotHole = await poteHolecollection.insertOne(newPothole);
 
-  if (insertedPotHole.acknowledged === true) {
-    console.log(newPothole);
-    return newPothole;
-  } else {
-    throw "this did not work!";
-  }
-};
-export default createPothole;
+//   if (insertedPotHole.acknowledged === true) {
+//     console.log(newPothole);
+//     return newPothole;
+//   } else {
+//     throw "this did not work!";
+//   }
+// };
+// export default createPothole;
