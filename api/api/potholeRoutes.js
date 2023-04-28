d4f3d7613cf2f69b039e85995bdcf83bbf358538
George Ayob <georgeayob21@gmail.com>
2023-04-28 19:09:07 -0400

    added more api calls for pot hole routes

--- api/api/potholeRoutes.js
+++ api/api/potholeRoutes.js
@@ -13,3 +13,33 @@ router.post("/create", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const result = await potholesController.GetPothole(id);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+  
+  router.patch("/assign/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { assignedTo } = req.body;
+      const result = await potholesController.AssignPotHoles(id, assignedTo);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+  
+  router.delete("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const result = await potholesController.DeletePothole(id);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
