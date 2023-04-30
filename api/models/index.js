e0613a7e1823d66458c8b3f1c77b8888ebfea0c9
Michael <miketheklein@gmail.com>
2023-04-30 09:53:49 -0700

    small fixes in user routes etc

--- api/models/index.js
+++ api/models/index.js
@@ -1,35 +1,34 @@
 import {
   userController,
   potholeController,
-  commentController,
+  commentController
 } from "../controllers/index.js";
 
-// import createPothole from "./potholes.js";
 
 (async () => {
-  // await userController.createUser(
-  //   "Jerell",
-  //   "Mendoza",
-  //   "Jerell@gpmail.org",
-  //   "Jerel1234!",
-  //   "user",
-  //   [],
-  //   "Admin",
-  //   "6441e1ed23c3324645e81114",
-  //   "6441e1ed23c3324645e81114",
-  //   "Jerell123456",
-  //   "05/01/1999"
-  // );
-  // await potholeController.CreatePotHole(
-  //   "6441e1ed23c3324645e81114",
-  //   "Open",
-  //   "https://www.cityworks.com/wp-content/uploads/2020/01/pot-hole-blog.gif",
-  //   "37.9101",
-  //   "122.0652",
-  //   "6441e1ed23c3324645e81114",
-  //   "94596",
-  //   []
-  // );
+  await userController.createUser(
+    "Jerell",
+    "Mendoza",
+    "Jerell@gpmail.org",
+    "Jerel1234!",
+    "user",
+    [],
+    "Admin",
+    [],
+    [],
+    "Jerell123456",
+    "05/01/1999"
+  );
+  await potholeController.CreatePotHole(
+    "6441e1ed23c3324645e81114",
+    "Open",
+    "https://www.cityworks.com/wp-content/uploads/2020/01/pot-hole-blog.gif",
+    "37.9101",
+    "122.0652",
+    "6441e1ed23c3324645e81114",
+    "94596",
+    []
+  );
   await commentController.CreateComment(
     "6441e1ed23c3324645e81114",
     "644df13eb3eade49c553cbf0",
