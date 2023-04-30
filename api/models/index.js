8b5e73d731d414dd7277ba539b50f2dfd1d58a07
Michael <miketheklein@gmail.com>
2023-04-29 21:45:50 -0700

    got all api routes adding to database.

--- api/models/index.js
+++ api/models/index.js
@@ -13,26 +13,26 @@ import {
   //   "Jerell@gpmail.org",
   //   "Jerel1234!",
   //   "user",
-  //   "This pothole is too much",
+  //   [],
   //   "Admin",
   //   "6441e1ed23c3324645e81114",
   //   "6441e1ed23c3324645e81114",
   //   "Jerell123456",
   //   "05/01/1999"
   // );
-  await potholeController.CreatePotHole(
-    "6441e1ed23c3324645e81114",
-    "Open",
-    "https://www.cityworks.com/wp-content/uploads/2020/01/pot-hole-blog.gif",
-    "37.9101",
-    "122.0652",
-    "6441e1ed23c3324645e81114",
-    "94596",
-    "[]"
-  );
+  // await potholeController.CreatePotHole(
+  //   "6441e1ed23c3324645e81114",
+  //   "Open",
+  //   "https://www.cityworks.com/wp-content/uploads/2020/01/pot-hole-blog.gif",
+  //   "37.9101",
+  //   "122.0652",
+  //   "6441e1ed23c3324645e81114",
+  //   "94596",
+  //   []
+  // );
   await commentController.CreateComment(
     "6441e1ed23c3324645e81114",
-    "644de16e1f556bafa829eef6",
+    "644df13eb3eade49c553cbf0",
     "Here is the pothole",
     "https://www.cityworks.com/wp-content/uploads/2020/01/pot-hole-blog.gif"
   );
